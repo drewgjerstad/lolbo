788698ba003f8f69b5cfb43c7ea0574d940119c6
drewgjerstad <drewgjerstad12@gmail.com>
2025-08-15 14:08:36 -0500

    Update analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -41,6 +41,43 @@ def process_output(fpath:str, task_id:str)->dict:
     return output
 
 
+def expand_output(oracle_calls:np.array, best_scores:np.array)->tuple:
+    """
+    Expand output to support running maximum best score found.
+
+    Args:
+        oracle_calls (np.array):
+            NumPy array containing "indexes" of oracle calls with new best.
+        best_scores (np.array):
+            NumPy array containing scores that improve on the previous scores.
+
+    Returns:
+        oracle_calls_exp (np.array):
+            Expanded oracle calls array.
+        best_scores_exp (np.array):
+            Expanded best scores found array.
+    """
+
+    oracle_calls_exp = np.arange(np.max(oracle_calls)) + 1  # 1-500
+    best_scores_exp = np.zeros_like(oracle_calls_exp)
+
+    current_best = 0.0
+    best_idx = 0
+    for i, oracle in enumerate(oracle_calls):
+        # Extract Best Score Found
+        best_score_found = best_scores[i]
+
+        # Fill In Current Best
+        for oracle_i in range(best_idx, oracle):
+            best_scores_exp[oracle_i] = current_best
+
+        # Update Current Best and Index
+        current_best = best_score_found
+        best_idx = oracle
+
+    return oracle_calls_exp, best_scores_exp
+
+
 def generate_plot(output:dict, best_in_set:float, title:str, xlab:str, ylab:str,
                   xlim:tuple, ylim:tuple, error_bars:bool=True,
                   fpath:str="plot.png", verbose:bool=True)->None:
@@ -76,12 +113,19 @@ def generate_plot(output:dict, best_in_set:float, title:str, xlab:str, ylab:str,
     num_oracle_calls = np.array(list(output.keys()))
     best_score_found = np.array(list(output.values()))
 
+    # Expand Output
+    oracles_exp, scores_exp = expand_output(oracle_calls=num_oracle_calls,
+                                           best_scores=best_score_found)
+
     # Initialize Figure
     fig, ax = plt.subplots(1, 1, figsize=(12, 10))
 
     # Plot Progress
-    ax.plot(num_oracle_calls, best_score_found, linestyle="--", marker="D",
-                markersize=8, color="b", label="LOLBO Progress")
+    ax.scatter(num_oracle_calls, best_score_found, marker="D",
+               color="b", label="LOL-BO Progress")
+    ax.plot(oracles_exp, scores_exp, linestyle="--", color="b", label=None)
+
+
 
     if error_bars:
         stderror = (np.std(best_score_found, ddof=1) /
@@ -92,7 +136,8 @@ def generate_plot(output:dict, best_in_set:float, title:str, xlab:str, ylab:str,
     # Plot Best in Set Line
     if best_in_set:
         ax.hlines(best_in_set, xmin=xlim[0], xmax=xlim[1], colors=["k"],
-                  linestyles=["dotted"], label="Best in Dataset")
+                  linestyles=["dotted"],
+                  label=f"Best in Dataset ({best_in_set})")
 
     # Plot Features
     ax.set_xlim(xlim[0], xlim[1])
@@ -114,6 +159,26 @@ def main():
     """
     Run analysis, reproduce LOLBO paper results.
     """
+
+    # Log p Task (512 max string length)
+    logp_512_fpath = "output_logp_512.txt"
+    logp_512_output = process_output(logp_512_fpath, task_id="logp")
+    generate_plot(output=logp_512_output,
+                  best_in_set=4.52,
+                  title="Penalized LogP (max_string_length=512)",
+                  xlab="Number of Steps",
+                  ylab="Best Score Found (Higher is Better)",
+                  xlim=(0, 500),
+                  ylim=(0, 600),
+                  fpath="plot_logp_512.png")
+
+    # Log p Task (default max string length [1024])
+
+    # Zaleplon MPO Task
+
+    # Perindopril MPO Task
+
+    # Ranolazine MPO Task
 
 if __name__ == "__main__":
     main()
