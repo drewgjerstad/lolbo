df5e76a3926805886444ae921166a7176bc0b6c7
drewgjerstad <drewgjerstad12@gmail.com>
2025-08-20 08:36:23 -0500

    Rerun zale task (prev. run timed out)

--- analysis/run_lolbo_zale.sh
+++ analysis/run_lolbo_zale.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
 # SETUP RESOURCE
-#SBATCH --time=12:00:00
+#SBATCH --time=20:00:00
 #SBATCH --ntasks=8
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gjers043@umn.edu
 #SBATCH -p v100
-#SBATCH --gres=gpu:v100:2
+#SBATCH --gres=gpu:v100:4
 #SBATCH --output=output_zale.txt
 
 source ~/.bashrc
@@ -15,4 +15,4 @@ conda activate lolbo-env
 
 cd /users/6/gjers043/lolbo/scripts/
 module load cuda
-CUDA_VISIBLE_DEVICES=0,1 python3 molecule_optimization.py --seed 1234 --task_id zale --max_string_length 400 --max_n_oracle_calls 120000 --bsz 10 - run_lolbo - done
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 molecule_optimization.py --seed 1234 --task_id zale --max_string_length 400 --max_n_oracle_calls 120000 --bsz 10 - run_lolbo - done
